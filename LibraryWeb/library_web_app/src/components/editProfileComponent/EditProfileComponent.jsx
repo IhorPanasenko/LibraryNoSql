1e49af30b30e315f2ab50698471987bd7a3c0a60
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-03 02:29:30 +0300

    Update EditProfileComponent.jsx

--- LibraryWeb/library_web_app/src/components/editProfileComponent/EditProfileComponent.jsx
+++ LibraryWeb/library_web_app/src/components/editProfileComponent/EditProfileComponent.jsx
@@ -1,28 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+
+const URL_UPDATEUSER = "https://localhost:7054/api/User/Update"
 
 function EditProfileComponent() {
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate();
+
+    var token = localStorage.getItem("token");
+    var decodedToken = jwtDecode(token);
+    console.log(decodedToken);
+    var id = decodedToken.Id;
+
+    async function update(e) {
+        e.preventDefault()
+        let body = {
+            Id: id,
+            login: login,
+            password: password
+        }
+
+        console.log(body);
+        await axios({
+            method: 'put',
+            url: URL_UPDATEUSER,
+            data: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json; charset=utf-8' }
+        }).then((response) => {
+            console.log(response.data)
+            navigate("/UserProfile");
+        })
+    }
+
     return (
-        <div className="m-5 bg-dark">
-            <Form>
-                <Form.Group className="m-3 bg-info" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+        <div className="m-5">
+            <h2>Enter new User Name and password</h2>
+            <Form className=' p-3 border border-dark m-3 bg-info'>
+                <Form.Group className="mb-4" controlId="formBasicEmail">
+                    <Form.Label>Login (User Name)</Form.Label>
+                    <Form.Control type="text" placeholder="Login" onChange={(e)=>setLogin(e.target.value)} />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-4" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+
+                <Button variant="primary" type="submit" onClick={(e) => update(e)}>
+                    Update
                 </Button>
             </Form>
         </div>
