2b79e27a7f7d32e14fe7e148bfb2bd666da4750b
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-03 02:29:33 +0300

    Update UserHeader.jsx

--- LibraryWeb/library_web_app/src/components/UserHeader/UserHeader.jsx
+++ LibraryWeb/library_web_app/src/components/UserHeader/UserHeader.jsx
@@ -1,17 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useNavigate } from 'react-router-dom';
 
 function UserHeader() {
+  const navigate = useNavigate()
+
+  function LogOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("Token");
+    navigate("/Login")
+  }
+
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/UserHome">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/UserHome">LibraryApp</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/UserHome">Home</Nav.Link>
             <Nav.Link href="/UserProfile">MyProfile</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick={() => LogOut()}>LogOut</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
