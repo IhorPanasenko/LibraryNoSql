a4ee3c649ba811a2b3d5dde7ffa8ad89da4f9289
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-02 07:12:26 +0300

    Create UserApiModel.cs

---
+++ LibraryNoSql/LibraryNoSql/ApiModels/UserApiModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryNoSql.ApiModels
+{
+    public class UserApiModel
+    {
+        public string Login { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
