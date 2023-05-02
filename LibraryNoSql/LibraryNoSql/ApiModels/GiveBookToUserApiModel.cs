e1fd7578708c6278a49ed3a7e7466fd963d8993c
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-02 07:14:08 +0300

    Create GiveBookToUserApiModel.cs

---
+++ LibraryNoSql/LibraryNoSql/ApiModels/GiveBookToUserApiModel.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson;
+
+namespace LibraryNoSql.ApiModels
+{
+    public class GiveBookToUserApiModel
+    {
+        public ObjectId bookId { get; set; }
+        public ObjectId userId { get; set; }
+    }
+}
