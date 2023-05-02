d431f0182d60d700f758574caa89f55489f585c8
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-02 07:13:50 +0300

    Create HttpContextExtensions.cs

---
+++ LibraryNoSql/LibraryNoSql/HttpContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace LibraryNoSql
+{
+    public static class HttpContextExtensions
+    {
+        public static string GetNameFromToken(this HttpContext context)
+        {
+            Claim? userClaim = context?.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name);
+            return userClaim?.Value;
+        }
+    }
+}
