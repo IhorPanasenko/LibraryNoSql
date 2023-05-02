8f7af9007fba60bdc29ce14e8c0d16a7ff88adcd
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-02 07:12:33 +0300

    Create IBookRepository.cs

---
+++ LibraryNoSql/LibraryNoSql/DAL/Interfaces/IBookRepository.cs
@@ -0,0 +1,22 @@
+﻿using LibraryNoSql.Models;
+using MongoDB.Bson;
+
+namespace LibraryNoSql.DAL.Interfaces
+{
+    public interface IBookRepository
+    {
+        public Book Insert(Book book);
+
+        public ICollection<Book> GetByUser(ObjectId userId);
+
+        public Book GetById(ObjectId id);
+
+        public IReadOnlyCollection<Book> GetAll();
+
+        public void Delete(ObjectId bookId);
+
+        public Book GiveBookToUser(ObjectId bookId, ObjectId userId);
+
+        public Book RetrieveBookFromUser(ObjectId bookId);
+    }
+}
