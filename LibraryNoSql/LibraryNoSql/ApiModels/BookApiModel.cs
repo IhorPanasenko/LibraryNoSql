0f8e4f6cad05207e606163ea1d5aafd5755469e6
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-02 07:11:40 +0300

    Create BookApiModel.cs

---
+++ LibraryNoSql/LibraryNoSql/ApiModels/BookApiModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryNoSql.ApiModels
+{
+    public class BookApiModel
+    {
+        public string Title { get; set; } = String.Empty;
+
+        public int Pages { get; set; }
+        public string Author { get; set; }=String.Empty;
+    }
+}
